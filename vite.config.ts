9518423b595070d2e9d4631bbc3bae42b945f8f2
Bernhard Schenkenfelder <bernhard.schenkenfelder@scch.at>
2026-07-13 19:50:29 +0000

    Edit vite.config.ts: add logo.svg to server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,7 @@ export default defineConfig({
       "/gen": "http://localhost:8787",
       "/firefox": "http://localhost:8787",
       "/docker": "http://localhost:8787",
+      "/logo.svg": "http://localhost:8787",
     },
   },
 });
